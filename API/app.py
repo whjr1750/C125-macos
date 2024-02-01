c4452e6fb3f772592981583c692ade27df7b2a58
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2024-02-01 18:24:14 +0530

    Update app.py

--- API/app.py
+++ API/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify, request
 from classifier import  get_prediction
+import ssl
 
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+# Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+# Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+  
 app = Flask(__name__)
 
 @app.route("/predict-digit", methods=["POST"])
